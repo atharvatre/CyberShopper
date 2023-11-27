052b814c83865c7c448995d9aa337cafefd5ea73
atharvatre <91801614+atharvatre@users.noreply.github.com>
2023-11-27 23:11:59 +0530

    display products according to category

--- controller/productController.js
+++ controller/productController.js
@@ -1,7 +1,7 @@
 import slugify from "slugify"
 import productModel from "../models/productModel.js"
 import fs from 'fs'
-
+import categoryModel from '../models/categoryModel.js'
 
 //create product controller
 export const createProductController = async (req, res) => {
@@ -283,6 +283,27 @@ export const relatedProductController = async (req, res) => {
             success: false,
             messaage: "error while getting similar products",
             error,
+        })
+    }
+}
+
+//get product by category
+
+export const productCategoryController = async (req, res) => {
+    try {
+        const category = await categoryModel.findOne({ slug: req.params.slug })
+        const products = await productModel.find({ category }).populate('category')
+        res.status(200).send({
+            success: true,
+            category,
+            products,
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({
+            success: false,
+            error,
+            messaage: 'Error while getting products by category'
         })
     }
 }
